627f2c37727f774f0598d8ba64f75f9fe16af832
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-10-11 20:43:09 +0100

    message component added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
     <h1>Peace</h1>
+    <message>Peace be Upon you !!</message>
   </div>
 </template>
 
 <script>
+import Message from './components/message.vue'
 export default {
   name: 'app',
+  components: {Message},
   data () {
     return {
     }
---
+++ src/components/message.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss">
+    .box {
+        background: #e3e3e3;
+        padding: 10px;
+        border: 1px sold #c5c5c5;
+    }
+</style>
