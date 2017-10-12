547dd562db1d72dbf63aa831511736dcf9353ad5
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-10-12 09:10:16 +0100

    vue-chartjs package added

--- src/components/chart.vue
+++ src/components/chart.vue
@@ -4,11 +4,7 @@
 </template>
 
 <script>
-    let TESTER = document.getElementById('tester');
-	Plotly.plot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
+
 export default {
   name: 'chart',
   data () {
